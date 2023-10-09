08d66dab00187ed699d26bd84ccb26569c8ce3e6
hb <106774638+BerthonHugo@users.noreply.github.com>
2023-10-09 18:35:01 +0200

    initial commit

---
+++ src/main/java/com/example/quizepf_backend/QuizEpfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizepf_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizEpfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizEpfBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quizepf_backend/QuizEpfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizepf_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizEpfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
